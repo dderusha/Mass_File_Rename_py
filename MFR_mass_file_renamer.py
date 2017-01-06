63c7d6fc563aaf1327e9eb30c2f2ae87a8f7258d
DDeRusha <dderusha@gmail.com>
2017-01-05 23:31:45 -0600

    Added comments
    original script is for a specific filename.  If you read the comments, you can use this script for any Mass File Renaming project.

--- MFR_mass_file_renamer.py
+++ MFR_mass_file_renamer.py
@@ -1,16 +1,21 @@
-#import os
-#[os.rename(f, f.replace('_', '-')) for f in os.listdir('.') if not f.startswith('.')]
-
-
+'''this script was originally written to change a batch of files all in the same folder / directory with the
+following names -   Gl.-013-gedenkteken-van-blauw-float-en-Azul-split.txt  the user wanted to remove the "."
+and move the number to the end of the filename.'''
 
 import os
 
+''' asks user what directory or folder they want files renamed'''
 directory = raw_input("Please Enter directory path. macOS - Drag folder (directory) into terminal: ")
+
+'''changes to the user inputted directory'''
 os.chdir(directory)
 
 for f in os.listdir(directory):
+    ''' f_name and f_ext are the front part of the file name and the extension, this splits them into two parts, nothing to change here'''
     f_name, f_ext = os.path.splitext(f)
-    #f_name.split('-')
+    ''' the following splits the file name into more pieces so we can rearrange the order, remove unwanted white space and "." that are
+    icluded in the text GI. which is referenced with f_gi .  each file part is split into a tuple. "f" just is my way of saying file
+     we need the underscore for no spaces and "gi" - GI, "num" - number'''
     f_gi, f_num, f_ged, f_van, f_bla, f_float, f_en, f_azu, f_spl, = f_name.split('-')
 
     f_gi = f_gi.strip()[:2]
@@ -25,15 +30,3 @@ for f in os.listdir(directory):
 
     re_named = '{}-{}-{}-{}-{}-{}-{}-{}-{}{}'.format(f_ged, f_van, f_bla, f_float, f_en, f_azu, f_spl, f_gi, f_num, f_ext)
     os.rename(f, re_named)
-
-
-'''paths = (os.path.join(root, filename)
-        for root, _, filenames in os.walk(directory)
-        for filename in filenames)
-
-for path in paths:
-    # the '#' in the example below will be replaced by the '-' in the filenames in the directory
-    newname = path.replace('.', '')
-    if newname != path:
-        os.rename(path, newname)
-print ("done")'''
